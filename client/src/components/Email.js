e7d8ea1dbd7285c408b31c3c538cc6c012be1d08
Michael Wenceslaus Putong <michael.putong@finaccel.co>
2024-01-05 16:52:22 +0700

    add input new email feature

--- client/src/components/Email.js
+++ client/src/components/Email.js
@@ -3,16 +3,29 @@ import axios from 'axios';
 
 export default function Email() {
   const [emails, setEmails] = useState([]);
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
     getEmailList()
-  }, []);
+  }, [emails]);
 
   const getEmailList = () => {
     axios.get('http://127.0.0.1:5000/emails')
       .then(res => {
         setEmails(res.data.data);
       })
+  }
+
+  const addEmail = (email) => {
+    axios.post('http://127.0.0.1:5000/emails', {
+        email: email,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   return (
@@ -23,6 +36,11 @@ export default function Email() {
           <li key={id}>{email}</li>
         ))}
       </ul>
+      <p>
+        <h2>New Email</h2>
+        <input value={email} onChange={e => setEmail(e.target.value)}/>
+        <button onClick={() => addEmail(email)}>Add</button>
+      </p>
     </>
   );
 }
